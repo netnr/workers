4616984ceb893ee3fa1ad9bb66c7bb1ae6ba2e3c
netnr <netnr@netnr.com>
2024-10-02 15:52:55 +0800

    wrangler v3 && loggly

--- cors/src/index.js
+++ cors/src/index.js
@@ -1,128 +1,105 @@
 /*
  * https://github.com/netnr/workers
  *
- * 2019-10-12 - 2022-06-28
+ * 2019-10-12 - 2024-10-02
  * netnr
  */
 
-addEventListener('fetch', event => {
-    event.passThroughOnException()
-    event.respondWith(handleRequest(event))
-})
-
-/**
-* Configurations
-*/
-const config = {
-    // 从 https://sematext.com/ 申请并修改令牌
-    sematextToken: "00000000-0000-0000-0000-000000000000",
-    // 是否丢弃请求中的 Referer，在目标网站应用防盗链时有用
-    dropReferer: false
-};
-
-/**
-* Respond to the request
-* @param {Request} request
-*/
-async function handleRequest(event) {
-    const { request } = event;
-
-    //请求头部、返回对象
-    let reqHeaders = new Headers(request.headers),
-        outBody, outStatus = 200, outStatusText = 'OK', outCt = null, outHeaders = new Headers({
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": reqHeaders.get('Access-Control-Allow-Headers') || "Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With, Token, x-access-token"
-        });
-
-    try {
-        //取域名第一个斜杠后的所有信息为代理链接
-        let url = request.url.substr(8);
-        url = decodeURIComponent(url.substr(url.indexOf('/') + 1));
-
-        //需要忽略的代理
-        if (request.method == "OPTIONS" || url.length < 3 || url.indexOf('.') == -1 || url == "favicon.ico" || url == "robots.txt") {
-            //输出提示
-            const invalid = !(request.method == "OPTIONS" || url.length === 0)
-            outBody = JSON.stringify({
-                code: invalid ? 400 : 0,
-                usage: 'Host/{URL}',
-                source: 'https://github.com/netnr/workers',
-                note: 'Blocking a large number of requests, please deploy it yourself'
+export default {
+    async fetch(request, _env, ctx) {
+        //请求头部、返回对象
+        let reqHeaders = new Headers(request.headers),
+            outBody, outStatus = 200, outStatusText = 'OK', outCt = null, outHeaders = new Headers({
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": reqHeaders.get('Access-Control-Allow-Headers') || "Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With, Token, x-access-token"
             });
+
+        try {
+            //取域名第一个斜杠后的所有信息为代理链接
+            let url = request.url.substr(8);
+            url = decodeURIComponent(url.substr(url.indexOf('/') + 1));
+
+            //需要忽略的代理
+            if (request.method == "OPTIONS" || url.length < 3 || url.indexOf('.') == -1 || url == "favicon.ico" || url == "robots.txt") {
+                //输出提示
+                const invalid = !(request.method == "OPTIONS" || url.length === 0)
+                outBody = JSON.stringify({
+                    code: invalid ? 400 : 0,
+                    usage: 'Host/{URL}',
+                    source: 'https://github.com/netnr/workers',
+                    note: 'Blocking a large number of requests, please deploy it yourself'
+                });
+                outCt = "application/json";
+                outStatus = invalid ? 400 : 200;
+            }
+            else {
+                url = fixUrl(url);
+
+                //构建 fetch 参数
+                let fp = {
+                    method: request.method,
+                    headers: {}
+                }
+
+                // 丢弃的头部信息 如 referer
+                const dropHeaders = ['content-length', 'content-type', 'host'];
+                let he = reqHeaders.entries();
+                for (let h of he) {
+                    const key = h[0], value = h[1];
+                    if (!dropHeaders.includes(key)) {
+                        fp.headers[key] = value;
+                    }
+                }
+
+                // 是否带 body
+                if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) >= 0) {
+                    const ct = (reqHeaders.get('content-type') || "").toLowerCase();
+                    if (ct.includes('application/json')) {
+                        fp.body = JSON.stringify(await request.json());
+                    } else if (ct.includes('application/text') || ct.includes('text/html')) {
+                        fp.body = await request.text();
+                    } else if (ct.includes('form')) {
+                        fp.body = await request.formData();
+                    } else {
+                        fp.body = await request.blob();
+                    }
+                }
+
+                // 发起 fetch
+                let fr = await fetch(url, fp);
+                outCt = fr.headers.get('content-type');
+                outStatus = fr.status;
+                outStatusText = fr.statusText;
+                outBody = fr.body;
+            }
+        } catch (err) {
             outCt = "application/json";
-            outStatus = invalid ? 400 : 200;
+            outBody = JSON.stringify({
+                code: -1,
+                msg: JSON.stringify(err.stack) || err
+            });
+            outStatus = 500;
         }
-        else {
-            url = fixUrl(url);
 
-            //构建 fetch 参数
-            let fp = {
-                method: request.method,
-                headers: {}
-            }
-
-            //保留头部其它信息
-            const dropHeaders = ['content-length', 'content-type', 'host'];
-            if (config.dropReferer) dropHeaders.push('referer');
-            let he = reqHeaders.entries();
-            for (let h of he) {
-                const key = h[0], value = h[1];
-                if (!dropHeaders.includes(key)) {
-                    fp.headers[key] = value;
-                }
-            }
-
-            // 是否带 body
-            if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) >= 0) {
-                const ct = (reqHeaders.get('content-type') || "").toLowerCase();
-                if (ct.includes('application/json')) {
-                    fp.body = JSON.stringify(await request.json());
-                } else if (ct.includes('application/text') || ct.includes('text/html')) {
-                    fp.body = await request.text();
-                } else if (ct.includes('form')) {
-                    fp.body = await request.formData();
-                } else {
-                    fp.body = await request.blob();
-                }
-            }
-
-            // 发起 fetch
-            let fr = (await fetch(url, fp));
-            outCt = fr.headers.get('content-type');
-            outStatus = fr.status;
-            outStatusText = fr.statusText;
-            outBody = fr.body;
+        //设置类型
+        if (outCt && outCt != "") {
+            outHeaders.set("content-type", outCt);
         }
-    } catch (err) {
-        outCt = "application/json";
-        outBody = JSON.stringify({
-            code: -1,
-            msg: JSON.stringify(err.stack) || err
-        });
-        outStatus = 500;
+
+        let response = new Response(outBody, {
+            status: outStatus,
+            statusText: outStatusText,
+            headers: outHeaders
+        })
+
+        //日志接口
+        logStack.add(ctx, request, response);
+
+        return response;
+        // return new Response('OK', { status: 200 })
     }
-
-    //设置类型
-    if (outCt && outCt != "") {
-        outHeaders.set("content-type", outCt);
-    }
-
-    let response = new Response(outBody, {
-        status: outStatus,
-        statusText: outStatusText,
-        headers: outHeaders
-    })
-
-    //日志接口
-    if (config.sematextToken != "00000000-0000-0000-0000-000000000000") {
-        sematext.add(event, request, response);
-    }
-
-    return response;
-
-    // return new Response('OK', { status: 200 })
-}
+};
 
 // 补齐 url
 function fixUrl(url) {
@@ -138,7 +115,9 @@ function fixUrl(url) {
 /**
 * 日志
 */
-const sematext = {
+const logStack = {
+    // Set your UUID https://www.loggly.com/
+    ts_UUID: "",
 
     /**
      * 构建发送主体
@@ -171,22 +150,22 @@ const sematext = {
             } catch { }
         }
 
-        return {
-            method: "POST",
-            body: JSON.stringify(body)
-        }
+        return body;
     },
 
     /**
      * 添加
-     * @param {any} event
+     * @param {any} ctx
      * @param {any} request
      * @param {any} response
      */
-    add: (event, request, response) => {
-        let url = `https://logsene-receiver.sematext.com/${config.sematextToken}/example/`;
-        const body = sematext.buildBody(request, response);
-
-        event.waitUntil(fetch(url, body))
+    add: (ctx, request, response) => {
+        if (logStack.ts_UUID.length) {
+            ctx.waitUntil(fetch(`http://logs-01.loggly.com/inputs/${logStack.ts_UUID}/tag/http/`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                body: JSON.stringify(logStack.buildBody(request, response)),
+            }));
+        }
     }
 };
